8f67587ede8c349e836005897c5b4caacad23896
Jessica Hong <jessicahong.24@gmail.com>
2019-03-14 19:10:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,17 +4,17 @@ function happyHolidays(){
 
 
 function happyHolidaysTo(name) {
-  return "Happy holidays, ${name}!"
+  return 'Happy holidays, ${name}!'
 }
 
 happyHolidayTo ("you")
 
 function happyHolidayTo (holiday, name) {
-  return "Happy ${holiday}, ${name}!"
+  return 'Happy ${holiday}, ${name}!'
 }
 happyHolidayTo ("Independence Day", "you")
 
 function holidayCountdown(holiday, days) {
-  return "It\'s ${days} days until ${holiday}!"
+  return 'It\'s ${days} days until ${holiday}!'
 }
 holidayCountdown ("Mother\'s Day", "20")
