d8047277d44da3acecff542b1d9b8f0aea415dfa
Jessica Hong <jessicahong.24@gmail.com>
2019-03-14 19:14:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function happyHolidays(){
 var name = "you"
 
 function happyHolidaysTo(name) {
-  return 'Happy holidays, ${name}!'
+  console.log ( 'Happy holidays, ${name}!'
 }
 
 happyHolidayTo ("you")
